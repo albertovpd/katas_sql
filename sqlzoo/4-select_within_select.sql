c5a153026a2b733c67f389c130ec3254df40c355
Vargas <a.vargas.pina@gmail.com>
2020-10-21 12:06:51 +0200

    6th done

--- sqlzoo/4-select_within_select.sql
+++ sqlzoo/4-select_within_select.sql
@@ -35,16 +35,45 @@ name;
 
 --4 Which country has a population that is more than Canada but less than Poland? 
 --Show the name and the population.
-
+select 
+name
+from world
+where
+population > 
+(
+select
+population
+from world
+where population = 'Canada'
+)
+and
+population <
+(
+select
+population
+from world
+where population = 'Poland'
+)
 
 --5 Show the name and the population of each country in Europe. 
 --Show the population as a percentage of the population of Germany.
-
+select
+name, 
+concat(round(100*population/ (select population from world where name='Germany'),0),'%') as percentage
+from world  
+where
+continent = 'Europe';
 
 --6 Which countries have a GDP greater than every country in Europe? 
 --[Give the name only.] (Some countries may have NULL gdp values) 
+select 
+name
+from world
+where gdp > all(select gdp from world where continent = 'Europe' and gdp>0)
+-- using ALL + column you must use the column > 0 at the end
+-- it also could have been done using min of every country in europe  
 
-
+  
 --7 Find the largest country (by area) in each continent, show the continent, the name and the area:  
 
 
